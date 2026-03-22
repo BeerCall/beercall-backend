e1c3d471d458a96f4d98b5c6d05a804d4c5e5275
migz3rrr <miguel.joubert@epitech.eu>
2026-03-22 14:07:58 +0100

    hotfix: provide get_db function

--- db/database.py
+++ db/database.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # On récupère l'URL depuis les variables d'environnement (configurées dans docker-compose)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
@@ -13,3 +13,13 @@ else:
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
