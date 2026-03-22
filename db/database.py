85fd1cd04d8d003f19ace24f53267e13e728cf6c
migz3rrr <miguel.joubert@epitech.eu>
2026-03-22 13:29:29 +0100

    feat: provide ci/cd conf and Dockerfile

--- db/database.py
+++ db/database.py
@@ -1,23 +1,15 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Pour Windows/Dev, on reste sur SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./beercall.db"
+# On récupère l'URL depuis les variables d'environnement (configurées dans docker-compose)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False} # Requis uniquement pour SQLite
-)
+# Configuration de l'engine
+if DATABASE_URL.startswith("postgresql"):
+    engine = create_engine(DATABASE_URL)
+else:
+    # Pour SQLite uniquement
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-# La fonction pour récupérer la session DB
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
